88e47d797da8f8d4c00efc08a4039ca38b3bc062
Zaenal Arifin <zaenal.ariefin24@gmail.com>
2025-07-10 08:20:57 +0700

    add 3 file

---
+++ TypeDataNumber.php
@@ -0,0 +1,33 @@
+<?php
+
+# Tipe Data Number int
+
+echo "Desimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(01234);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "Binary : ";
+var_dump(0b11111);
+
+echo "Underscore in number : ";
+var_dump(1_153_250_654);
+
+
+# Tipe Data Number floating point
+
+echo "Floating Point : ";
+var_dump(1.234);
+
+echo "Floating Point dengan E notation : (1.7 x 1000) : ";
+var_dump(1.7e3);
+
+echo "Floating Point dengan E notation minus : (1.7 x 0.001) : ";
+var_dump(1.7e-3);
+
+echo "Underscore di floating point : ";
+var_dump(1_123.123);
