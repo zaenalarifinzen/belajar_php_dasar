b6ca76dc13a13e8a3dec185ded1366dd9244325f
Zaenal Arifin <zaenal.ariefin24@gmail.com>
2025-07-11 13:23:04 +0700

    initial commit

--- CallbackFunction.php
+++ CallbackFunction.php
@@ -1,5 +1,13 @@
 <?php
 
 function sayHello(string $name, callable $filter) {
-    
+    $finalName = call_user_func($filter, $name);
+    echo "Hello $finalName" . PHP_EOL;
 }
+
+sayHello("Zaenal", "strtoupper");
+sayHello("Zaenal", "strtolower");
+sayHello("Zaenal", function(string $name) : string {
+    return strtoupper($name);
+});
+sayHello("Zaenal", fn($name) => strtoupper($name));
---
+++ Comment.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+    Ini adalah function untuk menampilkan tulisan Hello
+    kita bisa menambahkan banyak baris komentar disini
+*/
+function sayHello() {
+    // ini komentar satu baris
+    echo "Hello" . PHP_EOL;
+}
+
+sayHello();
